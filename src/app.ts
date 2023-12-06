ab59e1d03c0d57b3d0f959d97929f9f9e4893b34
Faiza Amjad <faizaam@student.chalmers.se>
2023-12-06 09:34:20 +0100

    #2 Add shared database cluster

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ import {
   MessagePayload,
 } from "./utilities/types-utils";
 import { MessageException } from "./exceptions/MessageException";
-const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Dentists";
+const mongoURI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://DIT356:gusdit356@clusterdit356.zpifkti.mongodb.net/Clinic?retryWrites=true&w=majority";
 const client = mqtt.connect(process.env.MQTT_URI || "mqtt://localhost:1883");
 
 const messageMapping: { [key: string]: MessageHandler } = {
@@ -16,7 +18,6 @@ const messageMapping: { [key: string]: MessageHandler } = {
   "dentists/me/:dentist_id": dentistController.getDentist,
   "dentists/update/:dentist_id": dentistController.updateDentist,
   "dentists/delete/:dentist_id": dentistController.deleteDentist,
-  
 };
 
 client.on("connect", () => {
@@ -27,13 +28,16 @@ client.on("message", async (topic, message) => {
   console.log(message.toString());
   const handler = messageMapping[topic];
   if (handler) {
-    const {payload,responseTopic,requestInfo} = JSON.parse(message.toString()) as MessagePayload;
+    const { payload, responseTopic, requestInfo } = JSON.parse(
+      message.toString()
+    ) as MessagePayload;
     try {
       console.log("message", message.toString());
-      const result = await handler(payload,requestInfo);
-      client.publish(responseTopic, JSON.stringify({data:result}), { qos: 2 });
+      const result = await handler(payload, requestInfo);
+      client.publish(responseTopic, JSON.stringify({ data: result }), {
+        qos: 2,
+      });
     } catch (error) {
-      
       if (error instanceof MessageException) {
         client.publish(
           responseTopic,
@@ -61,8 +65,6 @@ client.on("message", async (topic, message) => {
 
   //client.end();}
 });
-
-
 
 // Connect to MongoDB
 mongoose
